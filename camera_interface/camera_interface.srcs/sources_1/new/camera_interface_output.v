0e8659ae583ab320d9a5bc0b008712835659d9de
George <georgeyhere@gmail.com>
2020-12-24 15:36:35 -0800

    some code
    halfway done with output

---
+++ camera_interface/camera_interface.srcs/sources_1/new/camera_interface_output.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/24/2020 03:20:14 PM
+// Design Name: 
+// Module Name: camera_interface_output
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module camera_interface_output(
+
+    );
+endmodule
