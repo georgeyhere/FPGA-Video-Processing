fe0737aa190fdbff3a4f0830a8d5016002d62f5e
George <georgeyhere@gmail.com>
2021-01-10 17:51:12 -0800

    cleanup
    moved case(greyscale) out of case(row_count) in gaussian_control

--- Gaussian/Gaussian.srcs/sources_1/new/gaussian_control.v
+++ Gaussian/Gaussian.srcs/sources_1/new/gaussian_control.v
@@ -25,6 +25,7 @@
 module gaussian_control(
 input clk,
 input reset_n,
+input vsync,
 input [7:0] greyscale_value,
 input greyscale_valid,
 input minion0_ready,
@@ -43,6 +44,7 @@ output reg [10:0] minion2_target
 
     );
 
+reg [31:0] count;
 reg [10:0] row_count;
 reg [16:0] address;
 reg [15:0] byte_count;    
@@ -59,10 +61,7 @@ initial begin
     pixel_value <= 0;
     fsm_state <= 0;
     address <= 0;
-   
-    
     byte_count <= 0;
-    target_row <= 0;
     row_count <= 0;
 end
 
@@ -72,12 +71,10 @@ always@(posedge clk, negedge reset_n) begin
         pixel_address <= 0;
         pixel_value <= 0;
         pixel_valid <= 0;
-        fsm_state <= s0_idle;
         minion0_select <= 0;
         minion1_select <= 0;
         minion2_select <= 0;
         byte_count <= 0;
-        target_row <= 0;
         row_count <= 0;
     end
     else begin
@@ -91,172 +88,65 @@ always@(posedge clk, negedge reset_n) begin
                 minion1_select <= 0;
                 minion2_select <= 0;
                 byte_count <= 0;
-                target_row <= 0;
             end
             
             1: begin //greyscale_valid has gone high for the first time in the frame
                 minion0_select <= 1; //select minion 1
                 minion0_target <= 0; //set minion0 to compute row 0
-                case(greyscale_valid)
-                    0: begin
-                        pixel_address <= 0;
-                        pixel_value <= 0;
-                        pixel_valid <= 0;
-                    end
-                    1: begin //send the data until greyscale_valid is deasserted
-                        case(fsm_state) 
-                            s0_default: begin
-                                pixel_address <= byte_count * 8;
-                                pixel_value <= greyscale_value;
-                                pixel_valid <= 1;
-                                fsm_state <= s1_timer;
-                                count <= 7;
-                            end
-                            s1_timer: begin
-                                count <= (count == 0) ? 0:(count-1);
-                                fsm_state <= (count == 0) ? s0_default:s1_timer;
-                                byte_count <= (count == 0) ? (byte_count + 1):byte_count;
-                            end
-                        endcase
-                    end
-                endcase
             end
             
             2: begin //greyscale_valid has gone high for the second time in the frame
                 minion1_select <= 1; //minion0 is already selected, select minion1 as well
                 minion1_target <= 1; //set minion1 to compute row 1
-                case(greyscale_valid)
-                    0: begin
-                        pixel_address <= 0;
-                        pixel_value <= 0;
-                        pixel_valid <= 0;
-                    end
-                    1: begin //send the data until greyscale_valid is deasserted
-                        case(fsm_state) 
-                            s0_default: begin
-                                pixel_address <= byte_count * 8;
-                                pixel_value <= greyscale_value;
-                                pixel_valid <= 1;
-                                fsm_state <= s1_timer;
-                                count <= 7;
-                            end
-                            s1_timer: begin
-                                count <= (count == 0) ? 0:(count-1);
-                                fsm_state <= (count == 0) ? s0_default:s1_timer;
-                                byte_count <= (count == 0) ? (byte_count + 1):byte_count;
-                            end
-                        endcase
-                    end
-                endcase
             end
             
             3: begin //greyscale_valid has gone high for the third time in the frame
                 minion2_select <= 1; //select minion2, all minions currently receiving data
-                minion2_target <= 2;
-                case(greyscale_valid)
-                    0: begin
-                        pixel_address <= 0;
-                        pixel_value <= 0;
-                        pixel_valid <= 0;
-                    end
-                    1: begin //send the data until greyscale_valid is deasserted
-                        case(fsm_state) 
-                            s0_default: begin
-                                pixel_address <= byte_count * 8;
-                                pixel_value <= greyscale_value;
-                                pixel_valid <= 1;
-                                fsm_state <= s1_timer;
-                                count <= 7;
-                            end
-                            s1_timer: begin
-                                count <= (count == 0) ? 0:(count-1);
-                                fsm_state <= (count == 0) ? s0_default:s1_timer;
-                                byte_count <= (count == 0) ? (byte_count + 1):byte_count;
-                            end
-                        endcase
-                    end
-                endcase
+                minion2_target <= 2; //set minion2 to compute row 2
             end
             
             4: begin
-                minion0_select <= 0; //minion0 has had three rows now, deselect it
-                case(greyscale_valid)
-                    0: begin
-                        pixel_address <= 0;
-                        pixel_value <= 0;
-                        pixel_valid <= 0;
-                    end
-                    1: begin //send the data until greyscale_valid is deasserted
-                        case(fsm_state) 
-                            s0_default: begin
-                                pixel_address <= byte_count * 8;
-                                pixel_value <= greyscale_value;
-                                pixel_valid <= 1;
-                                fsm_state <= s1_timer;
-                                count <= 7;
-                            end
-                            s1_timer: begin
-                                count <= (count == 0) ? 0:(count-1);
-                                fsm_state <= (count == 0) ? s0_default:s1_timer;
-                                byte_count <= (count == 0) ? (byte_count + 1):byte_count;
-                            end
-                        endcase
-                    end
+                minion0_select <= 0; //minion0 has had three rows now, deselect it 
             end
             
             5: begin
                 minion1_select <= 0;
-                case(greyscale_valid)
-                    0: begin
-                        pixel_address <= 0;
-                        pixel_value <= 0;
-                        pixel_valid <= 0;
-                    end
-                    1: begin //send the data until greyscale_valid is deasserted
-                        case(fsm_state) 
-                            s0_default: begin
-                                pixel_address <= byte_count * 8;
-                                pixel_value <= greyscale_value;
-                                pixel_valid <= 1;
-                                fsm_state <= s1_timer;
-                                count <= 7;
-                            end
-                            s1_timer: begin
-                                count <= (count == 0) ? 0:(count-1);
-                                fsm_state <= (count == 0) ? s0_default:s1_timer;
-                                byte_count <= (count == 0) ? (byte_count + 1):byte_count;
-                            end
-                        endcase
-                    end
             end
             
             6: begin
                 minion2_select <= 0;
-                case(greyscale_valid)
-                    0: begin
-                        pixel_address <= 0;
-                        pixel_value <= 0;
-                        pixel_valid <= 0;
-                    end
-                    1: begin //send the data until greyscale_valid is deasserted
-                        case(fsm_state) 
-                            s0_default: begin
-                                pixel_address <= byte_count * 8;
-                                pixel_value <= greyscale_value;
-                                pixel_valid <= 1;
-                                fsm_state <= s1_timer;
-                                count <= 7;
-                            end
-                            s1_timer: begin
-                                count <= (count == 0) ? 0:(count-1);
-                                fsm_state <= (count == 0) ? s0_default:s1_timer;
-                                byte_count <= (count == 0) ? (byte_count + 1):byte_count;
-                            end
-                        endcase
-                    end
             end
-
         endcase
+        if(row_count > 0) begin 
+            case(greyscale_valid)
+                        0: begin
+                            pixel_address <= 0;
+                            pixel_value <= 0;
+                            pixel_valid <= 0;
+                        end
+                        1: begin //send the data until greyscale_valid is deasserted
+                            case(fsm_state) 
+                                s0_default: begin
+                                    pixel_address <= byte_count * 8;
+                                    pixel_value <= greyscale_value;
+                                    pixel_valid <= 1;
+                                    fsm_state <= s1_timer;
+                                    count <= 7;
+                                end
+                                s1_timer: begin
+                                    count <= (count == 0) ? 0:(count-1);
+                                    fsm_state <= (count == 0) ? s0_default:s1_timer;
+                                    byte_count <= (count == 0) ? (byte_count + 1):byte_count;
+                                end
+                            endcase
+                        end
+            endcase
+         end
+         else begin
+            pixel_address <= 0;
+            pixel_value <= 0;
+            pixel_valid <= 0;
+         end
     end
 end
 
